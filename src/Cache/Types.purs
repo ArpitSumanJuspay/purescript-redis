d0298a8fe3c31da95cd1de1c5580f7b83d3f9afc
Arpit Suman <arpit.suman@juspay.in>
2021-08-20 16:23:18 +0530

    updated encode and decode funcem

--- src/Cache/Types.purs
+++ src/Cache/Types.purs
@@ -2,20 +2,39 @@ module Cache.Types where
 
 import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff, kind Effect)
-import Data.BigInt (BigInt, toString)
+import Data.BigInt (BigInt, fromString, toString)
+import Data.Foreign.Class (class Decode, class Encode, decode, encode)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Eq (genericEq)
 import Data.Tuple (Tuple)
 import Prelude (class Eq, class Show, (<>))
+import Presto.Core.Utils.Encoding (defaultDecode, defaultEncode)
+import Data.Foldable (foldMap)
+import Data.Array (singleton)
+import Data.Bifoldable (bifoldMap)
 
-foreign import data CacheConn :: Type
+-- Represent a JavaScript object on which we can use standard Redis commands as
+-- functions. The intention is to represent both single-node and cluster
+-- connections as equivalent types that can be used with most functions. This
+-- doesn't work with Multi as that does not return the same value as these two
+-- types.
+class CacheConn a
+
+-- General types
+
+foreign import data SimpleConn :: Type
+
+instance simpleConnCacheConn :: CacheConn SimpleConn
 
 foreign import data CACHE :: Effect
 
-data CacheConnOpts
+data SimpleConnOpts
 
 type CacheEff e = Eff (cache :: CACHE | e)
 type CacheAff e = Aff (cache :: CACHE | e)
+
+
+-- Stream-related types
 
 data SetOptions = NoOptions
                 | IfNotExist
@@ -39,8 +58,6 @@ derive instance genericEntryID :: Generic EntryID _
 instance eqEntryID :: Eq EntryID where
     eq = genericEq
 
-data Entry = Entry EntryID (Array Item)
-
 instance showEntryID :: Show EntryID where
   show :: EntryID -> String
   show (EntryID ms seq) = toString ms <> "-" <> toString seq
@@ -50,7 +67,38 @@ instance showEntryID :: Show EntryID where
   show (MaxID         ) = "+"
   show (NewID         ) = ">"
 
+instance encodeEntryID :: Encode EntryID where 
+  encode (EntryID ms seq) = encode ((toString ms) <> " " <> (toString seq))
+  encode id = encode id
+
+instance decodeEntryID :: Decode EntryID where
+  decode id = decode id
+
+itemsToArray :: Array Item -> Array String
+itemsToArray = foldMap (bifoldMap singleton singleton)
+
+data Entry = Entry EntryID (Array Item)
+
+derive instance genericEntry :: Generic Entry _
+
+instance encodeEntry :: Encode Entry where
+  encode (Entry entryID arr) = encode (itemsToArray arr)
+
+instance decodeEntry :: Decode Entry where
+  decode entry = decode entry
+
 data TrimStrategy = Maxlen
 
 instance showTrimStrategy :: Show TrimStrategy where
   show Maxlen = "MAXLEN"
+
+
+-- Cluster-related types
+
+data ClusterConnOpts
+
+type ClusterHost = { port :: Int, host :: String }
+
+foreign import data ClusterConn :: Type
+
+instance clusterConnCacheConn :: CacheConn ClusterConn
